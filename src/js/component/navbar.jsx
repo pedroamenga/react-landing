6058258f24d9c8272d7292ce5d52a6033ad2c057
pedroamenga <pedroam92@gmail.com>
2022-04-14 14:35:16 +0000

    landing

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<div>
+			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button... bootstrap is working...
+			</a>
+			<p>
+				Made by{" "}
+				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+				love!
+			</p>
+		</div>
+	);
+};
+
+export default Home;
