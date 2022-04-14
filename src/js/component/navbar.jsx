a42dcdcb283c1c694c9f489abb4fa2bee4475a88
pedroamenga <pedroam92@gmail.com>
2022-04-14 19:19:14 +0000

    landing

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,20 +3,33 @@ import React from "react";
 const Navbar = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<nav className="navbar justify-content-end navbar-dark bg-dark pb-8">
+				<a className="navbar-brand" href="#">
+					Navbar
+				</a>
+				<ul className="nav">
+					<li className="nav-item text-white">
+						<a
+							className="nav-link active"
+							aria-current="page"
+							href="#">
+							Home
+						</a>
+					</li>
+					<li className="nav-item text-white">
+						<a className="nav-link" href="#">
+							About
+						</a>
+					</li>
+					<li className="nav-item text-white">
+						<a className="nav-link" href="#">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	);
 };
 
-export default Home;
+export default Navbar;
